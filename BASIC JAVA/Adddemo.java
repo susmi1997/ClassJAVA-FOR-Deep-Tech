cc9e967b47770973b0987764a347edf02e3073c4
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-15 00:05:54 +0530

    Create Adddemo.java

---
+++ BASIC JAVA/Adddemo.java
@@ -0,0 +1,13 @@
+package basicjava;
+import java.util.Scanner;
+public class Adddemo { //this is a demo for adding two integer
+       
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println(a+b);
+	}
+
+}
