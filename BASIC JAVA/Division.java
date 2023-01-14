01b6b1a0445a10e1e8a34bdaace110e865d14aa5
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-15 01:04:07 +0530

    Create Division.java

---
+++ BASIC JAVA/Division.java
@@ -0,0 +1,20 @@
+package basicjava;
+import java.util.Scanner;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int c,d;// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the value of c");
+        c=sc.nextInt();
+        
+        
+        System.out.println("Enter the value of d");
+        d=sc.nextInt();
+        System.out.println((float)c/(float)d);
+
+	}
+
+}
