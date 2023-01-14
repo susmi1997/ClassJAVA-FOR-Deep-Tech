1611d56fcfb4fc66902fd7b8c23c7d342c23616d
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-15 00:59:46 +0530

    Create VariableDemo.java

---
+++ BASIC JAVA/VariableDemo.java
@@ -0,0 +1,16 @@
+package basicjava;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+      int a=9,b=7;
+      float c=(float)a/(float)b; 
+      //Type casting method...
+      System.out.println("The value of C is="+c);
+      float d=9.5f;
+      int f=(int)d;
+      System.out.println("The value of F is=" +f);
+
+	}
+
+}
