79ecd16c0299ba74055cd0eb7b5ac21089ebda14
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-15 00:45:55 +0530

    Create SumOfTwoNumbers.java

---
+++ BASIC JAVA/SumOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package basicjava;
+
+public class SumOfTwoNumbers {
+
+ public static void main(String[] args) {
+
+  int a = 10;
+  int b = 20;
+
+  int sum = a +b;
+
+  System.out.println("Sum of " +a +"and" + b +"is"+sum);
+ }
+}
