255d3e937af8949a6f464485ff10965103397f6d
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-15 00:43:37 +0530

    Create AdditionOfTwoNumbers.java

---
+++ BASIC JAVA/AdditionOfTwoNumbers.java
@@ -0,0 +1,14 @@
+package basicjava;
+
+public class AdditionOfTwoNumbers {
+
+	public static void main(String[] args) {
+		
+       int a=60;
+       int b= 25;
+       int add = a+b;
+ System.out.println(a+b);
+       
+	}
+
+}
