dd19c71d2ec2a27396502753469af1e9188becfe
Susmita Mukherjee <98667361+susmi1997@users.noreply.github.com>
2023-01-15 01:02:09 +0530

    Create Multiplication.java

---
+++ BASIC JAVA/Multiplication.java
@@ -0,0 +1,15 @@
+package basicjava;
+import java.util.Scanner;
+public class Multiplication {
+
+	public static void main(String[] args) {
+		int a,b;
+		Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of a");
+		a=sc.nextInt();
+		System.out.println("Enter the value of b");
+        b=sc.nextInt();
+        System.out.println(a*b);
+	}
+
+}
